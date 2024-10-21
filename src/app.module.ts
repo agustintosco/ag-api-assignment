fce5bc69593cbafcf363061efc259c13b67f9f91
toscoagustinGR <agustin@greenrun.com>
2024-10-21 01:03:06 -0300

    removed autoschema geneation from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './domains/users/user.module';
 import { BetsModule } from './domains/bets/bet.module';
+import { SharedModule } from './domains/shared/shared.module';
 
 @Module({
   imports: [
@@ -15,10 +16,22 @@ import { BetsModule } from './domains/bets/bet.module';
     SequelizeModule.forRoot(sequelizeConfig),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      typePaths: ['./src/domains/graphql/schema.graphql'],
+      includeStacktraceInErrorResponses: false,
+      formatError: (error) => {
+        return {
+          message: error.message,
+          locations: error.locations,
+          path: error.path,
+          extensions: {
+            code: error.extensions.code,
+          },
+        };
+      },
     }),
     UsersModule,
     BetsModule,
+    SharedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
