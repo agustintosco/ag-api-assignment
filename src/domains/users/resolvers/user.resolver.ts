764a195eda1a756b1bf52ba634029c0b09cb886b
toscoagustinGR <agustin@greenrun.com>
2024-10-21 01:03:57 -0300

    updated user resolver

--- src/domains/users/resolvers/user.resolver.ts
+++ src/domains/users/resolvers/user.resolver.ts
@@ -1,23 +1,61 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  Int,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { User } from '../models/user.model';
 import { UserService } from '../services/user.service';
+import { handleError } from '../../utils/error-handler.util';
+import { Bet } from '../../bets/models/bet.model';
+import BetsLoaders from '../../bets/services/bet.loader';
+import { UserList } from '../interfaces/user.interfaces';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private betsLoader: BetsLoaders,
+  ) {}
 
   @Query(() => User)
   async getUser(@Args('id') id: number): Promise<User> {
     try {
-      return this.userService.findById(id);
+      const user = await this.userService.findByIdOrFail(id);
+
+      return user;
     } catch (error) {
-      // log error
-      throw error;
+      throw handleError(error);
     }
   }
 
-  @Query(() => [User])
-  async getUserList(): Promise<User[]> {
-    return this.userService.findAll();
+  @Query(() => UserList)
+  async getUserList(
+    @Args('limit', { type: () => Int, nullable: true }) limit: number = 10,
+    @Args('offset', { type: () => Int, nullable: true }) offset: number = 0,
+  ): Promise<{ users: User[]; hasNextPage: boolean }> {
+    try {
+      const users = await this.userService.findAll({ limit, offset });
+
+      const hasNextPage = users.length === limit;
+
+      return {
+        users,
+        hasNextPage,
+      };
+    } catch (error) {
+      throw handleError(error);
+    }
+  }
+
+  @ResolveField(() => [Bet])
+  async bets(@Parent() user: User): Promise<Bet[]> {
+    try {
+      return this.betsLoader.batchBets.load(user.id);
+    } catch (error) {
+      throw handleError(error);
+    }
   }
 }
